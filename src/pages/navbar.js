11affa89e98b92fe1d82769bfcd3a4d9f6a3d5a7
Samnoble118 <samnoble118@icloud.com>
2022-01-05 15:07:05 +0000

    updated to mobile nav bar

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './navbar.css'
 
 function navbar() {
+
+    // const [windowDimention, setWindowDimention] = useState(null);
+
+    // useEffect(() => {
+    //     setWindowDimention(window.innerWidth);
+    // }, []);
+
+    // useEffect(() => {
+    //     function handleResize() {
+    //         setWindowDimention(window.innerWidth);
+    //     }
+
+    //     window.addEventListener("resize", handleResize);
+    //     return () => window.removeEventListener("resize", handleResize);
+    //     }, [] );
+
+    //     const isMobile = windowDimention <= 640;
+
+    const burger = document.getElementsByClassName('burger')[0]
+    const navItems = document.getElementsByClassName('nav-items')[0]
+
+    burger.addEventListener('click', () => {
+    navItems.classList.toggle('active')
+    })
+
+
     return (
+
+        
+
+
         <div className="navbar">
             <h1>SN</h1>
+            <div className="burger">
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
             <ul className="nav-items">
                 <a href="#home-section"><li>Home</li></a>
                 <a href="#about-section"><li>About</li></a>
